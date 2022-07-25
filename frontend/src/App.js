cd22f2cc5fb2d588e210b8b4ca475114a8bac5a7
Ali <asadri721@gmail.com>
2022-07-25 05:44:38 +0430

    Create product page and rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from 'react-bootstrap';
 
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to ecommerce</h1>
+          <Home />
         </Container>
       </main>
       <Footer />
